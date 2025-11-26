bc4bbcabf4682faf18591ee8f63d1d7abb910b38
atulyasingh17 <atulyasingh017@gmail.com>
2025-11-26 12:42:52 +0530

    Create student.h

---
+++ include/student.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void showMenu();
+double convert(double amount, double rate);
+void clearScreen();
+void showCurrencies();
+double getRate(int from, int to);
